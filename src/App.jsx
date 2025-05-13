53b8c7eab15b40f5407c6ccf5d89e5ec91d2cf3e
swawiye <sovereign.wawiye@gmail.com>
2025-05-13 09:38:23 +0300

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './Style.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
 // Pages
 import Login from './pages/Login';
 import Signup from './pages/Signup';
@@ -7,8 +7,13 @@ import Signup from './pages/Signup';
 function App() {
   return (
     <Router>
+      <nav className='navbar'>
+        <NavLink className="nav-link" to="/">Home</NavLink>
+        <NavLink className="nav-link" to="/signup">Sign Up</NavLink>
+        <NavLink className="nav-link" to="/login">Log In</NavLink>
+      </nav>
       <Routes>
-        <Route path='/' element={<Signup />} />
+        <Route path='/signup' element={<Signup />} />
         <Route path='/login' element={<Login />} />
       </Routes>
     </Router>
