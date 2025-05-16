9c34323842d54fff36c559ac65ac07306521ae75
swawiye <sovereign.wawiye@gmail.com>
2025-05-16 10:53:20 +0300

    final touches

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+//Styling
 import './Style.css';
+
+//Connecting pages with react router
 import { BrowserRouter as Router, Routes, Route, NavLink } from 'react-router-dom';
+
 // Pages
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Home from './pages/Home';
+
 //Home page aftre you've logged in
 import ExpenseForm from './components/ExpenseForm';
+
+//Optimization before deployment
+//import React, { Suspense } from 'react';
 
 function App() {
   return (
